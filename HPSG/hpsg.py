cbb6cf540cb2f3606db01472342a43d1d2bb9091
rootofmylife <dotrananhduc@gmail.com>
2021-06-05 00:02:49 +0700

    Update hpsg.py

--- HPSG/hpsg.py
+++ HPSG/hpsg.py
@@ -8,16 +8,12 @@ def hpsg():
     dep = Dependency(DEP_PATH)
     dep.load()
 
-    # print(dep.word())
-    # print(dep.pos())
-    # print(dep.gold_pos())
-    # print(dep.head())
-    # print(dep.type())
-
     con = Constituency(CON_PATH, dep.head(), dep.type())
     con.load()
 
-    print(con.get_hpsg_tree())
+    # [[('NNP', 'Ms.'), ('NNP', 'Haag'), ('VBZ', 'plays'), ('NNP', 'Elianti'), ('.', '.')]]
+    return con.get_hpsg_tree()
+
 
 if __name__ == "__main__":
     hpsg()
