eddf7c004d20d8e4f45c521c401ab30d8188dcd8
rootofmylife <dotrananhduc@gmail.com>
2021-06-07 01:27:51 +0700

    update

--- HPSG/hpsg.py
+++ HPSG/hpsg.py
@@ -1,3 +1,8 @@
+import torch
+import numpy as np
+import itertools
+import time
+
 from dependency import Dependency
 from constituency import Constituency, InternalParseNode
 
@@ -7,6 +12,16 @@ from model.chart_parser import ChartParser
 
 DEP_PATH = "../samples/dependency.txt"
 CON_PATH = "../samples/constituency.txt"
+
+def format_elapsed(start_time):
+    elapsed_time = int(time.time() - start_time)
+    minutes, seconds = divmod(elapsed_time, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    elapsed_string = "{}h{:02}m{:02}s".format(hours, minutes, seconds)
+    if days > 0:
+        elapsed_string = "{}d{}".format(days, elapsed_string)
+    return elapsed_string
 
 def hpsg():
     dep = Dependency(DEP_PATH)
@@ -21,7 +36,7 @@ def hpsg():
 def train():
     parse_tree = hpsg().get_parse_tree()
 
-    print("Constructing vocabularies...")
+    print("Constructing vocabularies...\n")
     tag_vocab = Vocabulary()
     tag_vocab.index("<START>")
     tag_vocab.index("<STOP>")
@@ -82,10 +97,93 @@ def train():
     char_vocab.freeze() # ['\x00', '\x01', '\x02', '\x03', '\x04', .. ]
     type_vocab.freeze() # ['nsubj', 'det', 'nn']
 
-    print("Initializing model...")
+    print("Initializing model...\n")
     parser = ChartParser(tag_vocab, word_vocab, label_vocab, char_vocab, type_vocab)
 
-    print("Training model...")
+    print("Initializing optimizer...")
+    trainable_parameters = [param for param in parser.parameters() if param.requires_grad]
+    trainer = torch.optim.Adam(trainable_parameters, lr=1., betas=(0.9, 0.98), eps=1e-9)
+    warmup_coeff = 0.0008 / 160
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        trainer, 'max',
+        factor=0.5,
+        patience=5,
+        verbose=True,
+    )
+    grad_clip_threshold = np.inf
+
+    print("Training model...\n")
+    total_processed = 0
+    current_processed = 0
+    # check_every = len(parse_tree) / 4
+    best_dev_score = -np.inf
+    # best_model_path = None
+
+    start_time = time.time()
+
+    for epoch in itertools.count(start=1):
+        if epoch > 150: # Set epoch
+            break
+        
+        np.random.shuffle(parse_tree)
+        epoch_start_time = time.time()
+
+        for start_index in range(0, len(parse_tree), 250):
+            trainer.zero_grad()
+
+            iteration = total_processed // 250 + 1
+            if iteration <= 160:
+                for param_group in trainer.param_groups:
+                    param_group['lr'] = iteration * warmup_coeff
+
+            parser.train()
+
+            batch_loss_value = 0.0
+            batch_trees = parse_tree[start_index:start_index + 250]
+
+            batch_sentences = [[(leaf.tag, leaf.word) for leaf in tree.leaves()] for tree in batch_trees]
+            for subbatch_sentences, subbatch_trees in parser.split_batch(batch_sentences, batch_trees, 2000):
+                _, loss = parser.parse_batch(subbatch_sentences, subbatch_trees)
+
+                loss = loss / len(batch_trees)
+                loss_value = float(loss.data.cpu().numpy())
+                batch_loss_value += loss_value
+                if loss_value > 0:
+                    loss.backward()
+                del loss
+                total_processed += len(subbatch_trees)
+                current_processed += len(subbatch_trees)
+
+            grad_norm = torch.nn.utils.clip_grad_norm_(trainable_parameters, grad_clip_threshold)
+
+            trainer.step()
+
+            print(
+                "epoch {:,} "
+                "batch {:,}/{:,} "
+                "processed {:,} "
+                "batch-loss {:.4f} "
+                "grad-norm {:.4f} "
+                "epoch-elapsed {} "
+                "total-elapsed {}".format(
+                    epoch,
+                    start_index // 250 + 1,
+                    int(np.ceil(len(parse_tree) / 250)),
+                    total_processed,
+                    batch_loss_value,
+                    grad_norm,
+                    format_elapsed(epoch_start_time),
+                    format_elapsed(start_time),
+                )
+            )
+
+            # if current_processed >= check_every:
+            #     current_processed -= check_every
+            #     check_dev(epoch)
+
+        # adjust learning rate at the end of an epoch
+        if (total_processed // 250 + 1) > 160:
+            scheduler.step(best_dev_score)
 
     print("Finish training...")
 
--- HPSG/model/chart_parser.py
+++ HPSG/model/chart_parser.py
@@ -9,6 +9,11 @@ from model.encoder import Encoder
 from model.layer_normalization import LayerNormalization
 from model.dep_score import DepScore
 
+START = "<START>"
+STOP = "<STOP>"
+UNK = "<UNK>"
+TAG_UNK = "UNK"
+
 class ChartParser(nn.Module):
     def __init__(self, tag_vocab, word_vocab, label_vocab, char_vocab, type_vocab):
         super().__init__()
@@ -89,6 +94,158 @@ class ChartParser(nn.Module):
         self.dep_score = DepScore(type_vocab.size, annotation_dim)
         self.loss_func = torch.nn.CrossEntropyLoss(size_average=False)
         self.loss_funt = torch.nn.CrossEntropyLoss(size_average=False)
+
+    def split_batch(self, sentences, golds, subbatch_max_tokens=3000):
+        lens = [len(sentence) + 2 for sentence in sentences]
+
+        lens = np.asarray(lens, dtype=int)
+        lens_argsort = np.argsort(lens).tolist()
+
+        num_subbatches = 0
+        subbatch_size = 1
+        while lens_argsort:
+            if (subbatch_size == len(lens_argsort)) or (subbatch_size * lens[lens_argsort[subbatch_size]] > subbatch_max_tokens):
+                yield [sentences[i] for i in lens_argsort[:subbatch_size]], [golds[i] for i in lens_argsort[:subbatch_size]]
+                lens_argsort = lens_argsort[subbatch_size:]
+                num_subbatches += 1
+                subbatch_size = 1
+            else:
+                subbatch_size += 1
+
+    def parse_batch(self, sentences, golds=None):
+        is_train = golds is not None
+        self.train(is_train)
+        torch.set_grad_enabled(True)
+
+        if golds is None:
+            golds = [None] * len(sentences)
+
+        packed_len = sum([(len(sentence) + 2) for sentence in sentences])
+
+        i = 0
+        tag_idxs = np.zeros(packed_len, dtype=int)
+        word_idxs = np.zeros(packed_len, dtype=int)
+        batch_idxs = np.zeros(packed_len, dtype=int)
+        for snum, sentence in enumerate(sentences):
+            for (tag, word) in [(START, START)] + sentence + [(STOP, STOP)]:
+                tag_idxs[i] = 0 if (not self.use_tags and self.f_tag is None) else self.tag_vocab.index_or_unk(tag, TAG_UNK)
+                if word not in (START, STOP):
+                    count = self.word_vocab.count(word)
+                    if not count or (is_train and np.random.rand() < 1 / (1 + count)):
+                        word = UNK
+                word_idxs[i] = self.word_vocab.index(word)
+                batch_idxs[i] = snum
+                i += 1
+
+        batch_idxs = BatchIndices(batch_idxs)
+
+        emb_idxs_map = {
+            'tags': tag_idxs,
+            'words': word_idxs,
+        }
+        emb_idxs = [
+            from_numpy(emb_idxs_map[emb_type]).requires_grad_(False)
+            for emb_type in self.emb_types
+        ]
+        pre_words_idxs = from_numpy(word_idxs).requires_grad_(False)
+
+        extra_content_annotations_list = []
+        extra_content_annotations = None
+
+        if len(extra_content_annotations_list) > 1 :
+            if self.hparams.use_cat:
+                extra_content_annotations = torch.cat(extra_content_annotations_list, dim = -1)
+            else:
+                extra_content_annotations = sum(extra_content_annotations_list)
+        elif len(extra_content_annotations_list) == 1:
+            extra_content_annotations = extra_content_annotations_list[0]
+
+        annotations, self.current_attns = self.encoder(emb_idxs, pre_words_idxs, batch_idxs, extra_content_annotations=extra_content_annotations)
+
+        if self.partitioned and not self.use_lal:
+            annotations = torch.cat([
+                annotations[:, 0::2],
+                annotations[:, 1::2],
+            ], 1)
+
+        if self.use_lal and not self.lal_combine_as_self:
+            half_dim = self.lal_d_proj//2
+            d_l = self.label_vocab.size - 1
+            fencepost_annotations = torch.cat(
+                [annotations[:-1, (i*self.lal_d_proj):(i*self.lal_d_proj + half_dim)] for i in range(d_l)] 
+                + [annotations[1:, (i*self.lal_d_proj + half_dim):((i+1)*self.lal_d_proj)] for i in range(d_l)], 1)
+        else:
+            fencepost_annotations = torch.cat([
+                annotations[:-1, :self.d_model//2],
+                annotations[1:, self.d_model//2:],
+                ], 1)
+
+        fencepost_annotations_start = fencepost_annotations
+        fencepost_annotations_end = fencepost_annotations
+
+        fp_startpoints = batch_idxs.boundaries_np[:-1]
+        fp_endpoints = batch_idxs.boundaries_np[1:] - 1
+
+        if not is_train:
+            trees = []
+            scores = []
+            for i, (start, end) in enumerate(zip(fp_startpoints, fp_endpoints)):
+                tree, score = self.parse_from_annotations(fencepost_annotations_start[start:end,:],
+                                                                        fencepost_annotations_end[start:end,:], sentences[i], i)
+                trees.append(tree)
+                scores.append(score)
+
+            return trees, scores
+
+        pis = []
+        pjs = []
+        plabels = []
+        paugment_total = 0.0
+        cun = 0
+        num_p = 0
+        gis = []
+        gjs = []
+        glabels = []
+        with torch.no_grad():
+            for i, (start, end) in enumerate(zip(fp_startpoints, fp_endpoints)):
+
+                p_i, p_j, p_label, p_augment, g_i, g_j, g_label \
+                    = self.parse_from_annotations(fencepost_annotations_start[start:end,:], fencepost_annotations_end[start:end,:], sentences[i], i, gold=golds[i])
+
+                paugment_total += p_augment
+                num_p += p_i.shape[0]
+                pis.append(p_i + start)
+                pjs.append(p_j + start)
+                gis.append(g_i + start)
+                gjs.append(g_j + start)
+                plabels.append(p_label)
+                glabels.append(g_label)
+
+        cells_i = from_numpy(np.concatenate(pis + gis))
+        cells_j = from_numpy(np.concatenate(pjs + gjs))
+        cells_label = from_numpy(np.concatenate(plabels + glabels))
+
+        cells_label_scores = self.f_label(fencepost_annotations_end[cells_j] - fencepost_annotations_start[cells_i])
+        cells_label_scores = torch.cat([
+            cells_label_scores.new_zeros((cells_label_scores.size(0), 1)),
+            cells_label_scores
+        ], 1)
+        cells_label_scores = torch.gather(cells_label_scores, 1, cells_label[:, None])
+        loss = cells_label_scores[:num_p].sum() - cells_label_scores[num_p:].sum() + paugment_total
+
+        cun = 0
+        for snum, (start, end) in enumerate(zip(fp_startpoints, fp_endpoints)):
+            leng = end - start
+            arc_score, type_score = self.dep_score(fencepost_annotations_start[start:end,:], fencepost_annotations_end[start:end,:])
+            arc_gather = [leaf.father for leaf in golds[snum].leaves()]
+            type_gather = [self.type_vocab.index(leaf.type) for leaf in golds[snum].leaves()]
+            cun += 1
+            assert len(arc_gather) == leng - 1
+            arc_score = torch.transpose(arc_score,0, 1)
+            loss = loss + 0.5 * self.loss_func(arc_score[1:, :], from_numpy(np.array(arc_gather)).requires_grad_(False)) \
+                   + 0.5 * self.loss_funt(type_score[1:, :],from_numpy(np.array(type_gather)).requires_grad_(False))
+
+        return None, loss
 
 class BatchIndices:
     """
